7b7e4ae92cab36721f4fdd7f2af5565b4268db9a
Khushbu <khushbu.thakkar@outlook.com>
2021-10-06 16:21:48 -0700

    Refactor Application.js

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 
 export default function useApplicationData(){
 
+  
+
   const [state, setState] = useState({
     day: "Monday", 
     days:[],
@@ -16,6 +18,9 @@ export default function useApplicationData(){
   
   
 useEffect(() => {
+  const url = "ws://localhost:8000";
+  const webSocket = new WebSocket(url);
+//  webSocket.send('ping');
   Promise.all([
     axios.get('/api/days'),
     axios.get('/api/appointments'),
@@ -25,6 +30,7 @@ useEffect(() => {
     setState((prev) => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers:all[2].data }));
   //  console.log(all);
   });
+  return  webSocket.close();
 
 }, []);
 
